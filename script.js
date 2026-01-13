4d1d6847642232319c682a337c02b4d48a002ed0
wizcode2100 <tauhid2100@gmail.com>
2026-01-13 02:49:51 -0500

    Javascript03

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+let rahim;
+let karim;
+let rafiq;
+
+rahim = "cycle";
+karim = "car";
+rafiq = "walk";
+
+if(rahim == 'cycle'){
+
+    console.log("we will go to park");
+
+}
+
+if(karim == 'car'){
+
+    console.log("we will go to shop");
+}
+
+if(rafiq == 'walk'){
+
+    console.log("we will watch netflix");
+}
+
+else{
+
+    console.log("I will study");
+}
